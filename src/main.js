5d334c7ee80ce4bc5944939735e1bc66466f0fe1
alex_klymenko <klymenko.sasha21@gmail.com>
2020-06-19 18:30:26 +0300

    render all parts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const TASK_COUNT = 3;
+
 const createSiteMenuTemplate = () => {
   return (
     `<section class="control__btn-wrap">
@@ -357,3 +359,21 @@ const createLoadMoreButtonTemplate = () => {
 const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+render(siteHeaderElement, createSiteMenuTemplate());
+render(siteMainElement, createFilterTemplate());
+render(siteMainElement, createBoardTemplate());
+
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+const boardElement = siteMainElement.querySelector(`.board`);
+
+render(taskListElement, createTaskEditTemplate());
+
+for (let i = 0; i < TASK_COUNT; i++) {
+  render(taskListElement, createTaskTemplate());
+}
+
+render(boardElement, createLoadMoreButtonTemplate());
